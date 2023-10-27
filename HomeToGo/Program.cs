8160e9e3c72f7febb41b306b7e0a41c4fb175b22
Najma2110 <s364715@oslomet.no>
2023-10-27 15:48:35 +0200

    Revert "commit"
    This reverts commit 54200d63fe3988d019641be923dca361c2fde6a5.

--- HomeToGo/Program.cs
+++ HomeToGo/Program.cs
@@ -2,13 +2,9 @@ using Microsoft.EntityFrameworkCore;
 using HomeToGo.Models;
 using Microsoft.AspNetCore.Identity;
 using HomeToGo.Areas.Identity.Data;
-<<<<<<< Updated upstream
 using HomeToGo.DAL;
 using Serilog;
 using Serilog.Events;
-=======
-using Serilog; //dette burde stå i punkt, men det er en annen kode som står der4//
->>>>>>> Stashed changes
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ListingDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ListingDbContextConnection' not found.");
@@ -23,25 +19,12 @@ builder.Services.AddDbContext<ListingDbContext>(options => {
         builder.Configuration["ConnectionStrings:ListingDbContextConnection"]);
 });
 
-/*var loggerConfiguration = new LoggerConfiguration()
-    .MinimumLevel.Information() // levels: Trace< Information < Warning < Erorr < Fatal
-    .WriteTo.File($"Logs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log"); //Save to a different file according to time//
-
-loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
-                           e.Level == LogEventLevel.Information &&
-                           e.MessageTemplate.Text.Contains("Executed DbCommand"));
-   
-var logger = loggerConfiguration.CreateLogger();
-builder.Logging.AddSerilog(logger);
-
-*/
 builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ListingDbContext>();
 
 builder.Services.AddRazorPages(); 
 builder.Services.AddSession();
 
-<<<<<<< Updated upstream
 builder.Services.AddScoped<IListingRepository, ListingRepository>();
 
 var loggerConfiguration = new LoggerConfiguration()
@@ -54,9 +37,6 @@ loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceCont
 
 var logger = loggerConfiguration.CreateLogger();
 builder.Logging.AddSerilog(logger);
-=======
-// Er kodene fra Line 29 til og med 27 riktige?// 
->>>>>>> Stashed changes
 
 var app = builder.Build();
 
